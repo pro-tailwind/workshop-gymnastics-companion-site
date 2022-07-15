792d232437344648347782e55fd8653d6a441966
Simon Vrachliotis <simon@simonswiss.com>
2022-07-15 17:19:14 +1000

    not found component

--- components/not-found.jsx
+++ components/not-found.jsx
@@ -1,6 +1,6 @@
 export function NotFound() {
   return (
-    <div className="grid min-h-screen place-items-center bg-gradient-to-br from-orange-400 to-red-500">
+    <div className="grid h-full place-items-center bg-gradient-to-br from-orange-400 to-red-500">
       <p className="text-5xl font-semibold text-white">Oh no! We got a 404</p>
     </div>
   )
